b1cf6ea969eaa66c540eb3fe3044b1ce0d684e80
Nirmohi <76029713+Vedantjn@users.noreply.github.com>
2024-01-14 13:33:26 +0530

    Update Contact's index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_t8gg0hi', 'template_mmg2tdh', form.current, 'RWtWDn70kyuh83A-V')
       .then((result) => {
         setOpen(true);
         form.current.reset();
@@ -165,5 +165,4 @@ const Contact = () => {
     </Container>
   )
 }
-
 export default Contact
